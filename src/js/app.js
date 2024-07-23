f4bc57c5fdf1a00617d5d5b4cd510d5165aec7cc
lianet <jack.tolmak@yandex.ru>
2024-07-23 12:32:11 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -15,78 +15,39 @@ $(function () {
 
         let $target = $(e.target);
 
-        if ($target.is('.sidebar__caption-btn')) {
-            $target.addClass('active');
-            $target.next().slideDown()
-        }
-
-        if ($target.is('.sidebar__caption-item')) {
-            $('.sidebar__caption-btn').text($target.text()).removeClass('active').next().slideUp()
-        }
-
-        if ($target[0].closest(".sidebar__item-button")) {
-            $target.closest(".sidebar__item-button").next().slideToggle().parent().toggleClass('active');
-        }
-
-        if ($target.is('.prom-calendar__caption')) {
-
-            $('.prom-calendar__menu').not($target.next('.prom-calendar__menu')).slideUp();
-            $target.next('.prom-calendar__menu').slideToggle();
-
-            $('.prom-calendar__caption').not($target).removeClass('active');
-            $target.toggleClass('active');
-        }
-
     });
 
-    // init accordion
-    if ($('.prom-calendar__caption.active')) {
-        $('.prom-calendar__caption.active').next().slideDown(0)
+
+    if ($('.order-queue').length > 0) {
+
+        function syncTableRowHeights() {
+            var maxHeights = [];
+            $('table').each(function () {
+                var table = $(this);
+                var rows = table.find('tbody tr');
+
+
+                rows.each(function (index) {
+                    var row = $(this);
+                    var height = row.height();
+
+                    if (maxHeights[index] === undefined || height > maxHeights[index]) {
+                        maxHeights[index] = height;
+                    }
+                });
+
+
+                rows.each(function (index) {
+                    $(this).height(maxHeights[index]);
+                });
+            });
+        }
+
+
+        syncTableRowHeights();
+
+        $(window).on('resize', syncTableRowHeights);
     }
-
-
-
-
-    // split grid
-
-    let splitGrid = Split(['#split-1', '#split-2'], {
-        minSize: 0,
-        onDrag: () => {
-            $('.gutter').removeClass('gutter-open');
-            $('.prom-calendar').removeClass('full');
-            initalSizes = null;
-        }
-    });
-
-
-
-    let initalSizes;
-
-    $(document).on('dblclick', function (e) {
-        let $target = $(e.target);
-
-
-
-        if ($target.hasClass('gutter')) {
-
-            $target.toggleClass('gutter-open');
-            $('.prom-calendar').toggleClass('full');
-            if ($target.hasClass('gutter-open')) {
-                initalSizes = splitGrid.getSizes();
-                splitGrid.setSizes([0, 100])
-
-
-            } else {
-                $('.gutter').removeClass('gutter-open');
-                $('.prom-calendar').removeClass('full');
-                if (initalSizes) {
-                    splitGrid.setSizes(initalSizes);
-                    initalSizes = null;
-                }
-            }
-        }
-    });
-
 
 
     // simplebar
